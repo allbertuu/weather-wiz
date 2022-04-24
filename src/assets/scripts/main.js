a274f0509705378828dffaa3b5cdd2074073a5e0
Alberto <albertovinicius3@gmail.com>
2022-04-24 17:02:02 -0300

    add sass and moment.js

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,33 @@
+import moment from 'moment';
+
+let body = document.body;
+
+let currentTime = moment().format('LT');
+
+function getTimeFormatAM_PM() {
+  let lastIndex = currentTime[currentTime.length - 1];
+  let penultimateIndex = currentTime[currentTime.length - 2];
+  return penultimateIndex + lastIndex;
+}
+
+function getHour() {
+  let arrTime = currentTime.split(':');
+  let extractedHour = arrTime[0];
+  return extractedHour;
+}
+
+export function handleBodyStyles() {
+  let hour = getHour();
+  let timeFormat = getTimeFormatAM_PM();
+  
+  if(timeFormat === 'PM') {
+    if(hour >= '2') {
+      body.className = 'night';
+    }
+  }
+  else {
+    if(hour >= '6') {
+      body.classList.remove('night');
+    }
+  }
+}
