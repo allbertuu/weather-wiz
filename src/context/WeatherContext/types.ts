3f29b4bb229bd140426f7faf289c4d6ce3774208
Alberto <albertovinicius3@gmail.com>
2023-10-04 14:36:57 -0300

    chore: add open weather type

--- src/context/WeatherContext/types.ts
+++ src/context/WeatherContext/types.ts
@@ -1,10 +1,52 @@
+interface IOpenWeatherResponse {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
 export interface IWeatherProvider {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export interface IWeatherContext {
-    weatherData: any;
-    isGeolocationFound: boolean;
-    setIsGeolocationFound?: React.Dispatch<React.SetStateAction<boolean>>;
-    setWeatherData?: React.Dispatch<React.SetStateAction<any>>;
+  weatherData: IOpenWeatherResponse;
+  isGeolocationFound: boolean;
+  setIsGeolocationFound?: React.Dispatch<React.SetStateAction<boolean>>;
+  setWeatherData?: React.Dispatch<React.SetStateAction<IOpenWeatherResponse>>;
 }
