e8be90889086e9282ff73b6c8d3bf999ec00f71c
Alberto <albertovinicius3@gmail.com>
2025-11-03 17:45:06 -0300

    just format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,54 @@
-import { useEffect } from "react";
-import { handlePeriodOfTheDayBodyStyle } from "./utils";
-import "./styles/App.scss";
+import { useEffect } from 'react';
+import { handlePeriodOfTheDayBodyStyle } from './utils';
+import './styles/App.scss';
 import {
-    GeolocationInfos,
-    Greetings,
-    Spinner,
-    SupportCreator,
-    WeatherCard,
-} from "./components";
-import { useWeather } from "./hooks";
+  GeolocationInfos,
+  Greetings,
+  Spinner,
+  SupportCreator,
+  WeatherCard,
+} from './components';
+import { useWeather } from './hooks';
 
 function App() {
-    const { weatherData, isGeolocationFound } = useWeather();
+  const { weatherData, isGeolocationFound } = useWeather();
 
-    useEffect(() => {
-        handlePeriodOfTheDayBodyStyle();
-    }, []);
+  useEffect(() => {
+    handlePeriodOfTheDayBodyStyle();
+  }, []);
 
-    if (!isGeolocationFound) {
-        return (
-            <div className="container">
-                <h3>
-                    Você precisa habilitar <br />a localização no browser :)
-                </h3>
-            </div>
-        );
-    }
-
-    if (!weatherData) {
-        return (
-            <div className="container">
-                <h2>Observando o céu...</h2>
-                <Spinner />
-            </div>
-        );
-    }
-
+  if (!isGeolocationFound) {
     return (
-        <div className="container">
-            <SupportCreator />
-
-            <header className="header">
-                <Greetings />
-
-                <GeolocationInfos />
-            </header>
-
-            <WeatherCard />
-        </div>
+      <div className="container">
+        <h3>
+          Você precisa habilitar <br />a localização no browser :)
+        </h3>
+      </div>
     );
+  }
+
+  if (!weatherData) {
+    return (
+      <div className="container">
+        <h2>Observando o céu...</h2>
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <SupportCreator />
+
+      <header className="header">
+        <Greetings />
+
+        <GeolocationInfos />
+      </header>
+
+      <WeatherCard />
+    </div>
+  );
 }
 
 export default App;
