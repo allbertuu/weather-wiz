1e2bbd03d3d71da0d79c8926fd9a2297eb7803d1
Alberto <albertovinicius3@gmail.com>
2025-11-18 14:01:54 -0300

    🐞 fix(tests): incorrect use of `Chai` assertions
    To resolve this error when running the tests, I need to separate the imports from `@testing-library/jest-dom/vitest` into a specific test setup file and reference it in the Vite configuration file.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,1 @@
 /// <reference types="vite/client" />
-
-// Testes com Vitest e Testing Library
-import '@testing-library/jest-dom/vitest';
---
+++ vitest-setup.ts
@@ -0,0 +1,2 @@
+// Traz os tipos específicos do jest-dom para o Vitest
+import '@testing-library/jest-dom/vitest';
