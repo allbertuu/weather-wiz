87d923f53996117839ad6e3616a3b67f491167ec
Alberto <albertovinicius3@gmail.com>
2025-11-21 17:05:00 -0300

    🦄 refactor(geolocationinfos): keep component pure
    purity is very important for react and funcional programming

--- src/components/GeolocationInfos/index.tsx
+++ src/components/GeolocationInfos/index.tsx
@@ -1,17 +1,15 @@
 import { useEffect, useState } from 'react';
 import { formatHour } from '../../utils';
 import styles from './styles.module.scss';
-import { useWeather } from '../../hooks';
 
-const GeolocationInfos: React.FC<unknown> = () => {
-  const { weatherData } = useWeather();
-  const [currentHour, setCurrentHour] = useState<string>(
-    formatHour(new Date().getTime() / 1000),
-  );
-  const weatherDataUnknown = weatherData === null;
+const GeolocationInfos = ({ localName }: { localName?: string }) => {
+  // Formato: HH:MM
+  const defaultHour = formatHour(new Date().getTime() / 1000);
+  const [currentHour, setCurrentHour] = useState<string>(defaultHour);
+
+  const local = localName || 'algum lugar agradável';
 
   const FIFTEEN_SECONDS_IN_MILLISECONDS = 15000;
-  const localName = weatherData?.name;
 
   const updateHour = () => {
     setCurrentHour(formatHour(new Date().getTime() / 1000));
@@ -25,22 +23,13 @@ const GeolocationInfos: React.FC<unknown> = () => {
     };
   }, []);
 
-  if (weatherDataUnknown) {
-    return (
-      <p className={styles.container}>
-        {/* TODO: É possível obter a localização pelo Google Maps API, passando as
-          coordenadas.
-          https://developers.google.com/maps/documentation/geocoding/?hl=pt_BR#comecar
-         */}
-        Você está em <strong>algum lugar agradável</strong>, e são{' '}
-        <time className={styles.hour}>{currentHour}</time>
-      </p>
-    );
-  }
-
   return (
     <p className={styles.container}>
-      Você está em <strong>{localName}</strong>, e são{' '}
+      {/* TODO: É possível obter a localização pelo Google Maps API, passando as
+        coordenadas.
+        https://developers.google.com/maps/documentation/geocoding/?hl=pt_BR#comecar
+       */}
+      Você está em <strong>{local}</strong>, e são{' '}
       <time className={styles.hour}>{currentHour}</time>
     </p>
   );
